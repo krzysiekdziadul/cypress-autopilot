5b5346d3304ab924eb11b0b61810c4d96d1b9026
AzureAD\KrzysztofDziadul <Krzysztof.Dziadul@iamproperty.com>
2025-11-15 20:39:26 +0100

    config

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('test:after:run', (test, runnable) => {
-        // dodatkowe hooki jeśli potrzeba
-      })
     },
     reporter: 'mocha-junit-reporter',
     reporterOptions: {
