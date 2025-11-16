c6af66c7a2f0d98716cbaf398fad40f8f7775c59
AzureAD\KrzysztofDziadul <Krzysztof.Dziadul@iamproperty.com>
2025-11-16 18:45:24 +0100

    test 2

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -9,5 +9,10 @@ describe('example to-do app', () => {
     cy.get('.todo-list li').should('have.length', 2)
     cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  }),
+    it('displays demo failure', () => {
+    cy.get('.todo-list li').should('have.length', 1)
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 })
